a601604200c39fe1953654565f9eb01eb7cd4519
iambuddha <liniauskas@gmail.com>
2020-01-01 20:17:15 +0000

    adding all files

---
+++ aws/main.tf
@@ -0,0 +1,16 @@
+#----root/main.tf-----
+provider "aws" {
+  region = var.aws_region
+}
+
+# Deploy Storage Resources
+module "storage" {
+  source       = "./storage"
+  project_name = "${var.project_name}"
+}
+
+# Deploy Networking Resources
+module "networking" {
+  source       = "./networking"
+  vpc_cidr     = var.vpc_cidr
+}
---
+++ aws/terraform.tfvars
@@ -0,0 +1,4 @@
+aws_region   = "us-east-1"
+project_name = "myproject"
+vpc_cidr     = "10.123.0.0/16"
+
