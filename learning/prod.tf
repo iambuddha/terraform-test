5a5fd063eb240ce4cefb6464b55bc43a1c2584c9
Andrius <vyzg@yahoo.com>
2020-07-25 18:30:47 +0000

    add more instances

--- learning/prod.tf
+++ learning/prod.tf
@@ -38,6 +38,8 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
+  count = 2
+
   ami           = "ami-08f3d892de259504d"
   instance_type = "t2.micro"
 
@@ -50,11 +52,18 @@ resource "aws_instance" "prod_web" {
   }
 }
 
+resource "aws_eip_association" "prod_web" {
+  instance_id   = aws_instance.prod_web.0.id
+  allocation_id = aws_eip.prod_web.id
+}
+
 resource "aws_eip" "prod_web" {
-  instance = aws_instance.prod_web.id
 
   tags = {
     "Terraform" : "true"
-    Name = "Terra"
   }
 }
+
+
+
+
